866e07ef502dd0e6179b0b0999679176cf54b80c
Vineet Panchal <vineetpanchal03@gmail.com>
2025-11-29 00:19:41 -0500

    Update AddTwoNumbers.py

--- neetcode-150/linked-lists/AddTwoNumbers.py
+++ neetcode-150/linked-lists/AddTwoNumbers.py
@@ -68,30 +68,3 @@ def add(self, l1: ListNode, l2: ListNode, carry: int) -> ListNode:
 
 def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
     return self.add(l1, l2, 0)
-
-
-
-# Iteration
-# Time Complexity: 
-# Space Complexity: O(1) for extra space, O(max(n, m)) for the output list
-def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
-  dummy = ListNode()
-  cur = dummy
-
-  carry = 0
-  while l1 or l2 or carry:
-    v1 = l1.val if l1 else 0
-    v2 = l2.val if l2 else 0
-
-    # new digit
-    val = v1 + v2 + carry
-    carry = val // 10
-    val = val % 10
-    cur.next = ListNode(val)
-
-    # update ptrs
-    cur = cur.next
-    l1 = l1.next if l1 else None
-    l2 = l2.next if l2 else None
-
-  return dummy.next
